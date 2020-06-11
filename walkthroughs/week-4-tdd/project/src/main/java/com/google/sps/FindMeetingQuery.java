a5b25e662447c90e5490fb93b510fd88477d209e
nandinijalan1115 <nandini.jalan1115@gmail.com>
2020-06-11 14:10:00 +0000

    Update FindMeetingQuery.java file in project.

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,23 +1,75 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps;
-
+import java.util.*; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Collection;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+
+    Collection<TimeRange> collection = new ArrayList<TimeRange>();
+    int arr[] = new int[1441];
+    for(int i = 0; i < arr.length; i++) {
+        arr[i] = 0;
+    }
+    Collection<String> meeting = request.getAttendees();
+
+    Iterator<Event> itr = events.iterator();
+    TimeRange temptr;
+
+    while (itr.hasNext()){
+      Event event = (Event)itr.next();
+      Set<String> temp = event.getAttendees();
+      Iterator itrmeeting = meeting.iterator();
+      
+      while (itrmeeting.hasNext()){
+        String name = (String)itrmeeting.next();
+        
+        if (temp.contains(name)){
+          temptr = event.getWhen();
+          arr[temptr.start()]++;
+          arr[temptr.start()+temptr.duration()]--;
+        }
+      }
+
+    }
+    int count=0;
+    int start_time=0;
+    int flag=0;
+    for(int i = 1; i < arr.length; i++) {
+        arr[i] = arr[i]+arr[i-1];
+    }
+    if(arr[0]==0)
+    {
+        count=1;
+        start_time=0;
+    }
+    for(int i = 1; i < arr.length-1; i++) {
+        if(arr[i]==0)
+        {
+            if(arr[i-1]==0)
+            {
+                count++;
+            }
+            else
+            {
+                count=1;
+                start_time=i;
+            }
+        }
+        else
+        {
+            if(count>=request.getDuration())
+            collection.add(TimeRange.fromStartDuration(start_time, count));
+            count=0;
+        }
+    }
+    if(count>=request.getDuration())
+    collection.add(TimeRange.fromStartDuration(start_time, count));
+    return collection;
   }
 }
